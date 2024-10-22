29a1be558a0097104c7d4f4cff58ac514acc23db
frakoDev <frako23@gmail.com>
2024-10-22 12:49:15 -0400

    fixing issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/login/Login";
-import Home from "./pages/home/Home";
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
@@ -8,7 +7,6 @@ const App = () => {
     <div className="bg-black min-h-screen">
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
       </Routes>
       <Toaster />
     </div>
